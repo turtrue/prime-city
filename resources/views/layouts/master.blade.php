775e0683c7bbc8b4e6efea517914b61e7facaa04
Tural Kerimov <turaltrueandfalse@gmail.com>
2021-08-04 09:51:53 +0300

    update: mobile version

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link rel="stylesheet" href={{ asset("css/app.css") }}>
-    <link rel="icon" href={{ asset("img/favicon.ico") }}>
+    <link rel="icon" type="image/png" href="{{ asset("img/favicon.png") }}">
 </head>
 
 <body>
